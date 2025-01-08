f2a9c8b066e1573ed963d904c343e19e03c6a648
tghebu <cherkas.va.work2@gmail.com>
2025-01-08 20:54:17 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,15 @@
+username = 'Владислав'
+title = 'Лекция'
+content = 'Просмотр лекции по базовым структурам'
+status = 'В работе'
+created_date = '06-01-2025'
+issue_date = '07-01-2025'
+temp_created_date = 06.01
+temp_issue_date = 07.01
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', temp_created_date)
+print('Дата истечения заметки (дедлайн):', temp_issue_date)
